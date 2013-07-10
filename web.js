b05ac7d61e33693dd77b5c3506959b03472d0695
Brad Zeitner <bzeitner@gmail.com>
2013-07-10 04:02:43 +0000

    test

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var t = 'test';
+console.log("got request");
+
+var buffer = new Buffer(16);
+var rs = fs.readFile('index.html', function(err, data) {
+console.log(data.toString());
+	buffer.write(data.toString());
+});
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
